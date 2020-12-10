f47ad3dadb568df8b0273436fedd3858ac825b8f
Gustavo Alberto Valladares Sagrero <gvalladares@ucol.mx>
2020-12-10 15:52:50 -0600

    Implemented factorial() function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-export default class App {}
+export default class App {
+
+    factorial(numero)
+    {
+        let total = 1;
+        for (let i=1; i<= numero; i++)
+        {
+            total*=i;
+        }
+
+        return total;
+
+    }
+
+
+
+}
 
 let app = new App();
+
+console.log(app.factorial(5));
+
